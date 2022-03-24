ced0ec11d8b3798e362ae04ed94ed666df5cdc8e
Alex Sumaqie <22001926@campus.unicaen.fr>
2022-03-24 16:27:13 +0100

    travail

--- CTP/app/controllers/StoreController.php
+++ CTP/app/controllers/StoreController.php
@@ -1,11 +1,14 @@
 <?php
 namespace controllers;
+use Ubiquity\attributes\items\router\Post;
 use Ubiquity\attributes\items\router\Get;
 
 use models\Product;
 use models\Section;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
+
 /**
  * Controller StoreController
  */
@@ -21,6 +24,32 @@ class StoreController extends \controllers\ControllerBase{
 	public function OtherProducts($id){
         $prod = DAO::getById(Section::class,"id=".$id);
 		$this->loadView('StoreController/OtherProducts.html',compact('prod'));
+
+	}
+
+
+	#[Post(path: "Store/addtocart/{idproduct}/{count}",name: "store.addtocart")]
+	public function addtocart($idproduct,$count){
+        $prod = DAO::getById(Section::class,"id=".$idproduct,"count = .$count");
+		$this->index();
+
+        if(USession::get("checkout") == null){
+            USession::set("checkout",[[$idproduct=>1]]);
+
+
+        }else{
+            $pannier = USession::get("checkout");
+            $pannier = array_merge($pannier,[[$idproduct=>1]]) ;
+            USession::set("checkout",$pannier);
+        }
+
+	}
+
+
+	#[Route(path: "Store/allProducts",name: "store.allProducts")]
+	public function allProducts(){
+		
+		$this->loadView('StoreController/allProducts.html');
 
 	}
 
